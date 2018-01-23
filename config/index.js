b19ccb2500e5c85ebc05574a4eadb972db99711a
e <649998464@qq.com>
2018-01-23 12:42:57 +0800

    show

--- config/index.js
+++ config/index.js
@@ -50,7 +50,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/mfw_pc',
+    assetsPublicPath: '/mfw_pcshow',
 
     /**
      * Source Maps
